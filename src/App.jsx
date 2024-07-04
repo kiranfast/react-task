9d7e0602b7f4a71fc2db4ec2b06344fc45a32f03
kiranfast <kiranfast008@gmail.com>
2024-07-04 17:51:36 +0530

    task

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className="pricing">
+  <div className="container">
+    <div className="row">
+
+      <div className="col-lg-4">
+        <div className="card mb-5 mb-lg-0">
+          <div className="card-body">
+            <h5 className="card-title text-muted text-uppercase text-center">Free</h5>
+            <h6 className="card-price text-center">$0<span className="period">/month</span></h6>
+            <hr/>
+            <ul className="card">
+              <li> <span className="fa-li"><i className="fas fa-check"></i></span>  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>Single User</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg> 5GB Storage</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>Unlimited Public Projects</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg> Community Access</li>
+              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>Unlimited
+                Private Projects</li>
+              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>Dedicated
+                Phone Support</li>
+              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>Free Subdomain
+              </li>
+              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>Monthly Status
+                Reports</li>
+            </ul>
+            <div className="d-grid">
+              <a href="#" className="btn btn-primary text-uppercase">Button</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-4">
+        <div className="card mb-5 mb-lg-0">
+          <div className="card-body">
+            <h5 className="card-title text-muted text-uppercase text-center">Plus</h5>
+            <h6 className="card-price text-center">$9<span className="period">/month</span></h6>
+            <hr/>
+            <ul className="card">
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>5 Users</strong></li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>50GB Storage</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>Unlimited Public Projects</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>Community Access</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>Unlimited Private Projects</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>Dedicated Phone Support</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>Free Subdomain</li>
+              <li className="text-muted"><span className="fa-li"><i className="fas fa-times"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>Monthly Status
+                Reports</li>
+            </ul>
+            <div className="d-grid">
+              <a href="#" className="btn btn-primary text-uppercase">Button</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-4">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title text-muted text-uppercase text-center">Pro</h5>
+            <h6 className="card-price text-center">$49<span className="period">/month</span></h6>
+            <hr/>
+            <ul className="card">
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>Unlimited Users</strong>
+              </li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>150GB Storage</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>Unlimited Public Projects</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>Community Access</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>Unlimited Private Projects</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>Dedicated Phone Support</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><strong><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>Unlimited</strong> Free
+                Subdomains</li>
+              <li><span className="fa-li"><i className="fas fa-check"></i></span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>Monthly Status Reports</li>
+            </ul>
+            <div className="d-grid">
+              <a href="#" className="btn btn-primary text-uppercase">Button</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    </div>
+  )
+}
+
+export default App
